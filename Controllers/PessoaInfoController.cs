3740340882e90ebd0917e52647b0f1be9aa3a8e8
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2020-03-13 09:25:27 -0300

    Initial commit

---
+++ Controllers/PessoaInfoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TP3REAL.Models;
+
+namespace TP3REAL.Controllers
+{
+    public class PessoaInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(PessoaInfoModel pessoaInfoModel)
+        {
+
+            return View("Result",pessoaInfoModel);
+        }
+    }
+}
---
+++ Models/PessoaInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP3REAL.Models
+{
+    public class PessoaInfoModel
+    {
+        public string Nome { get; set; }
+       
+        public string Sobrenome { get; set; }
+
+        public DateTime DataNiver { get; set; }
+    }
+}
